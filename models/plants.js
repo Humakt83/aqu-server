580233b79488752cec4da80e9623c8ea676b2322
Humakt83 <urlact@yahoo.com>
2016-09-16 10:20:01 +0300

    Update plant schema to include water values

--- models/plants.js
+++ models/plants.js
@@ -14,6 +14,31 @@ var plantsSchema = new Schema({
     height: {
         type: Number,
         required: true
+    },
+    temperatureMin: {
+        type: Number,
+        required: true
+    },
+    temperatureMax: {
+        type: Number,
+        required: true
+    },
+    phMin: {
+        type: Number,
+        required: true
+    },
+    phMax: {
+        type: Number,
+        required: true
+    },
+    dhMin: {
+        type: Number,
+        required: true,
+        default: 0        
+    },
+    dhMax: {
+        type: Number,
+        required: true
     }
 });
 
