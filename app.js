fda4ed19e94bbb3ca0d9be16f792ad98e015e6c5
Humakt83 <urlact@yahoo.com>
2016-09-17 01:30:32 +0300

    Add cors headers

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Add headers
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
+
 app.use('/plants', plantsRouter);
 
 // catch 404 and forward to error handler
@@ -54,5 +62,9 @@ app.use(function(err, req, res, next) {
 });
 
 require('./dbinsert/plantsInserter')();
+
+var listener = app.listen(8888, function(){
+    console.log('Listening on port ' + listener.address().port);
+});
 
 module.exports = app;
